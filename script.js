a90dd57c37e7cac69c947d2a5d2abc8268573c88
Apekshya Dhakal <apekshya.d@gmail.com>
2022-10-06 16:43:15 -0400

    changing grid size on user input

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const container = document.querySelector('#container');
 let drawMode = false;
+let slider = document.querySelector('#myRange');
+let output = document.querySelector('#output');
+let size = slider.value ** 2;
+output.innerHTML = slider.value;
+draw();
 
-for (let i = 0; i < 256; i++){
+slider.addEventListener('change',(e)=>{
+    output.innerHTML = e.target.value;
+    size = e.target.value ** 2;
+    container.style.gridTemplateColumns = `repeat(${e.target.value}, 1fr)`;
+    removeChildNodes()
+    draw();
+});
+
+function draw() {
+    for (let i = 0; i < size; i++){
     let cell = document.createElement('div');
     container.appendChild(cell);
     cell.addEventListener('click', ()=>{drawMode = !drawMode;});
@@ -9,4 +23,14 @@ for (let i = 0; i < 256; i++){
         if(drawMode){cell.style.backgroundColor = 'black';};
     } );
 }
+}
+
+function removeChildNodes(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+
 
